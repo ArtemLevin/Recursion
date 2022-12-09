d8f40c5d109d9b78f89b614421032f329c51fabc
ArtemLevin <python_study_2022@mail.ru>
2022-12-10 02:14:23 +0300

    RecGCD, RecFib, RecSum

---
+++ Rec_Euk/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter the first number: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int B = int.Parse(Console.ReadLine());
+
+int Euklid(int A, int B)
+{
+    if (B == 0)
+    {
+        return A;
+    }
+    else
+    {
+        int C = A%B;
+        return Euklid(B, C);
+    }
+}
+
+Console.Write($"GCD is {Euklid(A, B)} ");
---
+++ Rec_Fib/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter the number of members of the sequence ");
+int number = int.Parse(Console.ReadLine());
+
+int Fib(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    if (number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (Fib(number-1) + Fib(number-2));
+    }
+}
+
+
+Console.Write($"The value of the {number} term of the sequence is {Fib(number)} ");
+
