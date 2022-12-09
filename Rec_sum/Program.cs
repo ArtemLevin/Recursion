2a734ee15ae273ed20033843d0b3641c76935a04
ArtemLevin <python_study_2022@mail.ru>
2022-12-10 00:48:03 +0300

    RecSum

---
+++ Rec_sum/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("enter the number of terms: ");
+int number = int.Parse(Console.ReadLine());
+
+int Terms(int num)
+{
+
+    if (num <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return num + Terms(num - 1);
+    }
+}
+
+Console.Write("The sum of terms is " + Terms(number));
+
