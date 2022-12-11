13d937a6993785ca5b7d8081cfe55ab095c23284
ArtemLevin <python_study_2022@mail.ru>
2022-12-11 20:59:51 +0300

    RecSQRT

---
+++ Rec_SQRT/Program.cs
@@ -0,0 +1,28 @@
+﻿int i = 0;
+double Sqrty(int num)
+{
+
+    if (i == 1000)
+    {
+        return 10;
+    }
+    else
+    {
+        i++;
+        return Math.Sqrt(num + Sqrty(num));
+    }
+}
+
+// Console.Write("enter a number: ");
+// int number = int.Parse(Console.ReadLine());
+
+int k = 1;
+while (k<21)
+{   
+    i = 0;
+    Console.Write("k =" + k );
+    Console.WriteLine(" " + Sqrty(k));
+    k ++;
+}
+
+
