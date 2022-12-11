b8af1a25ba63f3c296a2720869238a21844c03d3
ArtemLevin <python_study_2022@mail.ru>
2022-12-11 17:51:11 +0300

    Added SQRT

---
+++ Rec_DecToBin/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+int Binary(int num)
+{
+    if (num == 0)
+    {
+        Console.Write(num);
+        return 0;
+        
+    }
+    if (num == 1)
+    {
+        Console.Write(num);
+        return 0;
+        
+    }
+    else
+    {
+        int rem = num%2;
+        
+        Console.Write(rem);
+        
+        return (rem + Binary(num/2));
+    }
+}
+
+Binary(number);
+
