e46f49f3c3aeda2d85c5c69f65c5a73e98863144
ArtemLevin <python_study_2022@mail.ru>
2022-12-24 14:53:24 +0300

    Сумма прогрессии

---
+++ AriphmeticProgression/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int SumArithmeticProgression (int theFirstMemberOfTheProgression, int theLastMemberOfTheProgression )
+
+{
+    int sum = 0;
+    if (theFirstMemberOfTheProgression == theLastMemberOfTheProgression ) return sum;
+    else 
+    {
+        sum += theFirstMemberOfTheProgression*theFirstMemberOfTheProgression;
+        theFirstMemberOfTheProgression++;
+        return sum + SumArithmeticProgression(theFirstMemberOfTheProgression,theLastMemberOfTheProgression );
+    }
+}
+
+Console.WriteLine(SumArithmeticProgression (1, 6));
+
